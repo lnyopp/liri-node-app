fe82c9ac86f56bafdf9f231e9ed37851fc1fea80
Rumble Roar <lnyopp15@gmail.com>
2018-10-31 14:24:45 -0400

    added switch cases, and instructions for use of liribot, will be work on functions next

--- liri-node-app/liri.js
+++ liri-node-app/liri.js
@@ -1,2 +1,27 @@
-js
-  var spotify = new Spotify(keys.spotify);
+require("dotenv").config();
+
+var keys = require("./keys.js");
+var Spotify = require("node-spotify-api");
+// var spotify = new Spotify(keys.spotify);
+
+var liriReturn = process.argv[2];
+
+switch (liriReturn) {
+    case "concert-this":
+        concertThis();
+        break;
+    case "spotify-this-song":
+        spotifyThisSong();
+        break;
+    case "movie-this":
+        movieThis();
+        break;
+    case "do-what-it-says":
+        doWhatItSays();
+        break;
+
+    default: console.log(`liribot can search for information on your favorite songs,possible concerts, and movies. Just use the following commands after 'node liri.js' :
+    'spotify-this-song' followed by any song title you choose
+    'movie-this' followed by any movie title
+    *make sure to use quotes for multi-word titles* `)
+};
